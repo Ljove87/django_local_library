3b238af73c8806b52900ecee2b1c7f2f9c3721ad
Ljove87 <veljko33614@its.edu.rs>
2019-02-10 19:12:16 +0100

    First vestion of app moved to github

---
+++ catalog/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+
+from catalog.models import Author, Genre, Book, BookInstance
+
+
+admin.site.register(Genre)
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    list_display = ('imprint','status', 'borrower', 'due_back', 'id')
+    fieldsets = (
+        ('Book Info', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': [('status', ('due_back', 'borrower'))]
+        }),
+    )
